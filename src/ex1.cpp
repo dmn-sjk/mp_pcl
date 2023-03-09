30ed3624580292f1cfa66a474d457d3b82168ac2
dmn-sjk <sojka.damian9@gmail.com>
2023-03-09 19:14:44 +0100

    initial

---
+++ src/ex1.cpp
@@ -0,0 +1,82 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/Image.h>
+// PCL specific includes
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include <math.h>
+
+ros::Publisher pub;
+
+void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input) {
+    // Create a container for the data.
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::fromROSMsg(*input, *cloud);
+
+    float SCANNER_COLS = 1024;
+    float SCANNER_ROWS = 128;
+    
+    // Create range image
+    cv::Mat range_img = cv::Mat(SCANNER_ROWS, SCANNER_COLS, CV_32FC1);
+    range_img = 0;
+
+    for (int idx = 0; idx < cloud->points.size(); idx++) {
+        // Get point from cloud
+        pcl::PointXYZI point = cloud->points[idx];
+
+        // Remove invalid points
+        if (!std::isfinite(point.x) ||
+            !std::isfinite(point.y) ||
+            !std::isfinite(point.z))
+            continue;
+            
+        // TODO: Calculate horizontal and vertical angle of the laser beam
+
+
+        // TODO: Calculate the range value
+        float range = 0;
+
+
+        // TODO: Calculate the pixel location in the image                                                 
+        float height = 0;
+        float width = 0;
+
+
+        // TODO: Limit the range value
+    
+
+        // Set the range value in the pixel of an image
+        range_img.at<float>(height, width) = range;
+    }
+
+    // Convert image to CV_8UC1
+    range_img.convertTo(range_img, CV_8UC1);
+
+    // TODO: Normalize range_img to 0 - 255
+    // cv::normalize();
+
+    cv_bridge::CvImage out_msg;
+    out_msg.header   = input->header; // Same timestamp and tf frame as input image
+    out_msg.encoding = sensor_msgs::image_encodings::TYPE_8UC1;
+    out_msg.image    = range_img;
+
+    pub.publish(out_msg.toImageMsg()); // publish range image
+}
+
+int main (int argc, char** argv) {
+    // Initialize ROS
+    ros::init(argc, argv, "pc_to_range_image");
+    ros::NodeHandle nh;
+
+    // Create a ROS subscriber for the input point cloud
+    ros::Subscriber sub = nh.subscribe("os_cloud_node/points", 1, cloud_cb);
+
+    // Create a ROS publisher for the output point cloud
+    pub = nh.advertise<sensor_msgs::Image>("range_image", 1);
+
+    // Spin
+    ros::spin();
+}
---
+++ src/ex2.cpp
@@ -0,0 +1,81 @@
+#include <ros/ros.h>
+// PCL specific includes
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+
+ros::Publisher cones_pub;
+ros::Publisher processed_cloud_pub;
+
+void filter(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud) {
+    // TODO
+}
+
+void remove_ground(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud) {
+    // TODO
+}
+
+void get_cluster_indices(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud,
+                         std::vector<pcl::PointIndices> &output_cluster_indices) {
+    // TODO
+}
+
+void get_cones_centers_cloud(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud,
+                             const std::vector<pcl::PointIndices> &cluster_indices,
+                             const pcl::PointCloud<pcl::PointXYZ>::Ptr &output_cones_cloud) {
+    // TODO
+}
+
+void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input) {
+    // Create a container for the data.
+    sensor_msgs::PointCloud2 output;
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr centroid_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+    pcl::fromROSMsg(*input, *pcl_cloud);
+
+    int cloud_size = pcl_cloud->points.size();
+    ROS_INFO("Cloud size before filtering: %d", cloud_size);
+
+    filter(pcl_cloud);
+
+    cloud_size = pcl_cloud->points.size();
+    ROS_INFO("Cloud size after filtering: %d\n", cloud_size);
+
+    remove_ground(pcl_cloud);
+
+    std::vector<pcl::PointIndices> cluster_indices;
+    get_cluster_indices(pcl_cloud, cluster_indices);
+    get_cones_centers_cloud(pcl_cloud, cluster_indices, centroid_cloud);
+
+    // Publish processed cloud
+    pcl::toROSMsg(*pcl_cloud, output);
+    output.header = input->header;
+    output.fields = input->fields;
+    processed_cloud_pub.publish(output);
+
+    // Publish cone centers
+    pcl::toROSMsg(*centroid_cloud, output);
+    output.header = input->header;
+    output.fields = input->fields;
+    cones_pub.publish(output);
+}
+
+int main (int argc, char** argv) {
+    // Initialize ROS
+    ros::init(argc, argv, "cones_detector");
+    ros::NodeHandle nh;
+
+    // Create a ROS subscriber for the input point cloud
+    ros::Subscriber sub = nh.subscribe("fsds/lidar/Lidar", 1, cloud_cb);
+
+    // Create a ROS publisher for the output point clouds
+    cones_pub = nh.advertise<sensor_msgs::PointCloud2>("cones_cloud", 1);
+    processed_cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("processed_cloud", 1);
+
+    // Spin
+    ros::spin();
+}
