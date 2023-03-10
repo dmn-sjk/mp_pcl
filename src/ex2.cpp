d514bbdc0deb757cfc846866d2a9e0580b054a08
dmn-sjk <sojka.damian9@gmail.com>
2023-03-10 10:40:53 +0100

    fix

--- src/ex2.cpp
+++ src/ex2.cpp
@@ -5,6 +5,15 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/radius_outlier_removal.h>
+#include <pcl/filters/passthrough.h>
+
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/segmentation/extract_clusters.h>
 
 ros::Publisher cones_pub;
 ros::Publisher processed_cloud_pub;
